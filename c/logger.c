0e82611fc550648789ccd7c4ab7622f8200d151d
oxygen0605 <ozon0605@gmail.com>
2021-05-15 13:13:19 +0900

    style: コメントの追加。出力コメントの修正。

--- c/logger.c
+++ c/logger.c
@@ -8,11 +8,11 @@ void closeLogfile();
 MyLogger logger = {NULL, openLogfile, closeLogfile};
 
 void openLogfile(uint8_t* fname){
-    if (!logger.pfile) logger.pfile = fopen(fname, "a");
+    if (!logger.pfile) logger.pfile = fopen(fname, "a"); // overwrite
     fprintf(logger.pfile, "========= Logging Start ============\n");
 }
 
 void closeLogfile(){
-    fprintf(logger.pfile, "========= Logging End ============\n");
+    fprintf(logger.pfile, "========= Logging End ==============\n");
     if(logger.pfile) fclose(logger.pfile);
 }
