ec13aa6a872d250c98ee7bc9c4530127ca538ec4
oxygen0605 <ozon0605@gmail.com>
2021-05-18 17:05:47 +0900

    feat: 即時書き込みのためのfflushを追加。ファイルをフルパスで表示しない仕様に変更。

--- c/logger.h
+++ c/logger.h
@@ -4,6 +4,9 @@
 #include <stdio.h>
 //#include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
+
+#define FILE_NAME (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 
 typedef struct MyLogger {
     FILE* pfile;
@@ -14,25 +17,25 @@ extern MyLogger logger;
 
 /* File Log MACRO */
 #ifdef FDBG_ON
-#define FLOGE(...) fprintf(logger.pfile, "ERR:%s(%d) %s:", __FILE__, __LINE__, __func__), fprintf(logger.pfile, __VA_ARGS__)
-#define FLOGW(...) fprintf(logger.pfile, "WRN:%s(%d) %s:", __FILE__, __LINE__, __func__), fprintf(logger.pfile, __VA_ARGS__)
-#define FLOGI(...) fprintf(logger.pfile, "INF:%s(%d) %s:", __FILE__, __LINE__, __func__), fprintf(logger.pfile, __VA_ARGS__)
-#define FLOGD(...) fprintf(logger.pfile, "DBG:%s(%d) %s:", __FILE__, __LINE__, __func__), fprintf(logger.pfile, __VA_ARGS__)
+#define FLOGE(...) fprintf(logger.pfile, "ERR:%s(%d) %s:", FILE_NAME, __LINE__, __func__), fprintf(logger.pfile, __VA_ARGS__); fflush(logger.pfile); 
+#define FLOGW(...) fprintf(logger.pfile, "WRN:%s(%d) %s:", FILE_NAME, __LINE__, __func__), fprintf(logger.pfile, __VA_ARGS__); fflush(logger.pfile);
+#define FLOGI(...) fprintf(logger.pfile, "INF:%s(%d) %s:", FILE_NAME, __LINE__, __func__), fprintf(logger.pfile, __VA_ARGS__); fflush(logger.pfile);
+#define FLOGD(...) fprintf(logger.pfile, "DBG:%s(%d) %s:", FILE_NAME, __LINE__, __func__), fprintf(logger.pfile, __VA_ARGS__); fflush(logger.pfile);
 
 #elif FINF_ON
-#define FLOGE(...) fprintf(logger.pfile, "ERR:%s(%d) %s:", __FILE__, __LINE__, __func__), fprintf(logger.pfile, __VA_ARGS__)
-#define FLOGW(...) fprintf(logger.pfile, "WRN:%s(%d) %s:", __FILE__, __LINE__, __func__), fprintf(logger.pfile, __VA_ARGS__)
-#define FLOGI(...) fprintf(logger.pfile, "INF:%s(%d) %s:", __FILE__, __LINE__, __func__), fprintf(logger.pfile, __VA_ARGS__)
+#define FLOGE(...) fprintf(logger.pfile, "ERR:%s(%d) %s:", FILE_NAME, __LINE__, __func__), fprintf(logger.pfile, __VA_ARGS__); fflush(logger.pfile);
+#define FLOGW(...) fprintf(logger.pfile, "WRN:%s(%d) %s:", FILE_NAME, __LINE__, __func__), fprintf(logger.pfile, __VA_ARGS__); fflush(logger.pfile);
+#define FLOGI(...) fprintf(logger.pfile, "INF:%s(%d) %s:", FILE_NAME, __LINE__, __func__), fprintf(logger.pfile, __VA_ARGS__); fflush(logger.pfile);
 #define FLOGD(...) 
 
 #elif FWRN_ON
-#define FLOGE(...) fprintf(logger.pfile, "ERR:%s(%d) %s:", __FILE__, __LINE__, __func__), fprintf(logger.pfile, __VA_ARGS__)
-#define FLOGW(...) fprintf(logger.pfile, "WRN:%s(%d) %s:", __FILE__, __LINE__, __func__), fprintf(logger.pfile, __VA_ARGS__)
+#define FLOGE(...) fprintf(logger.pfile, "ERR:%s(%d) %s:", FILE_NAME, __LINE__, __func__), fprintf(logger.pfile, __VA_ARGS__); fflush(logger.pfile);
+#define FLOGW(...) fprintf(logger.pfile, "WRN:%s(%d) %s:", FILE_NAME, __LINE__, __func__), fprintf(logger.pfile, __VA_ARGS__); fflush(logger.pfile);
 #define FLOGI(...) 
 #define FLOGD(...) 
 
 #elif FERR_ON
-#define FLOGE(...) fprintf(logger.pfile, "ERR:%s(%d) %s:", __FILE__, __LINE__, __func__), fprintf(logger.pfile, __VA_ARGS__)
+#define FLOGE(...) fprintf(logger.pfile, "ERR:%s(%d) %s:", FILE_NAME, __LINE__, __func__), fprintf(logger.pfile, __VA_ARGS__); fflush(logger.pfile);
 #define FLOGW(...) 
 #define FLOGI(...) 
 #define FLOGD(...) 
@@ -48,25 +51,25 @@ extern MyLogger logger;
 
 /* Print Log MACRO */
 #ifdef PDBG_ON
-#define PLOGE(...) fprintf(stdout, "ERR:%s(%d) %s:", __FILE__, __LINE__, __func__), printf(__VA_ARGS__)
-#define PLOGW(...) fprintf(stdout, "WRN:%s(%d) %s:", __FILE__, __LINE__, __func__), printf(__VA_ARGS__)
-#define PLOGI(...) fprintf(stdout, "INF:%s(%d) %s:", __FILE__, __LINE__, __func__), printf(__VA_ARGS__)
-#define PLOGD(...) fprintf(stdout, "DBG:%s(%d) %s:", __FILE__, __LINE__, __func__), printf(__VA_ARGS__)
+#define PLOGE(...) fprintf(stdout, "ERR:%s(%d) %s:", FILE_NAME, __LINE__, __func__), printf(__VA_ARGS__)
+#define PLOGW(...) fprintf(stdout, "WRN:%s(%d) %s:", FILE_NAME, __LINE__, __func__), printf(__VA_ARGS__)
+#define PLOGI(...) fprintf(stdout, "INF:%s(%d) %s:", FILE_NAME, __LINE__, __func__), printf(__VA_ARGS__)
+#define PLOGD(...) fprintf(stdout, "DBG:%s(%d) %s:", FILE_NAME, __LINE__, __func__), printf(__VA_ARGS__)
 
 #elif PINF_ON
-#define PLOGE(...) fprintf(stdout, "ERR:%s(%d) %s:", __FILE__, __LINE__, __func__), printf(__VA_ARGS__)
-#define PLOGW(...) fprintf(stdout, "WRN:%s(%d) %s:", __FILE__, __LINE__, __func__), printf(__VA_ARGS__)
-#define PLOGI(...) fprintf(stdout, "INF:%s(%d) %s:", __FILE__, __LINE__, __func__), printf(__VA_ARGS__)
+#define PLOGE(...) fprintf(stdout, "ERR:%s(%d) %s:", FILE_NAME, __LINE__, __func__), printf(__VA_ARGS__)
+#define PLOGW(...) fprintf(stdout, "WRN:%s(%d) %s:", FILE_NAME, __LINE__, __func__), printf(__VA_ARGS__)
+#define PLOGI(...) fprintf(stdout, "INF:%s(%d) %s:", FILE_NAME, __LINE__, __func__), printf(__VA_ARGS__)
 #define PLOGD(...) 
 
 #elif PWRN_ON
-#define PLOGE(...) fprintf(stdout, "ERR:%s(%d) %s:", __FILE__, __LINE__, __func__), printf(__VA_ARGS__)
-#define PLOGW(...) fprintf(stdout, "WRN:%s(%d) %s:", __FILE__, __LINE__, __func__), printf(__VA_ARGS__)
+#define PLOGE(...) fprintf(stdout, "ERR:%s(%d) %s:", FILE_NAME, __LINE__, __func__), printf(__VA_ARGS__)
+#define PLOGW(...) fprintf(stdout, "WRN:%s(%d) %s:", FILE_NAME, __LINE__, __func__), printf(__VA_ARGS__)
 #define PLOGI(...) 
 #define PLOGD(...) 
 
 #elif PERR_ON
-#define PLOGE(...) fprintf(stdout, "ERR:%s(%d) %s:", __FILE__, __LINE__, __func__), printf(__VA_ARGS__)
+#define PLOGE(...) fprintf(stdout, "ERR:%s(%d) %s:", FILE_NAME, __LINE__, __func__), printf(__VA_ARGS__)
 #define PLOGW(...) 
 #define PLOGI(...) 
 #define PLOGD(...) 
